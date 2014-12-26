37a678e81201799418f004d2467701e8d75f9aea
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2014-12-25 22:54:27 -0500

    Create startup.sh

---
+++ startup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+if [ -f /etc/configured ]; then
+  echo 'already configured'
+else
+  #code that need to run only one time ....
+  /usr/bin/mysqld_safe &
+  sleep 5s
+  MYSQL_PASSWD=${MYSQL_PASSWD:-mysqlpsswd}
+  mysqladmin -u root password ${MYSQL_PASSWD}
+  killall mysqld
+  sleep 5s
+  #needed for fix problem with ubuntu and cron
+  update-locale
+  date > /etc/configured
+fi
